f2da2bf57aa3fe695899a19732a80580c6cec746
febalci <febalci@yahoo.com>
2019-10-01 12:49:24 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycrowipmodule',
-    version='0.18',
+    version='0.19',
     description='Library for Crow/AAP Alarm Ip Module',
     url='https://github.com/febalci/pycrowipmodule',
-    maintainer='febalci',
+    author='febalci',
+    author_email='febalci@yahoo.com',
     license='MIT',
     packages=find_packages(),
     install_requires=['requests'],
